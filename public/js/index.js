2f3b75cfd9d5f34469cbc0fbc36bceae62031361
harsanyizoli <h.zoli098@gmail.com>
2019-10-10 13:06:18 +0200

    idk

--- public/js/index.js
+++ public/js/index.js
@@ -11,7 +11,6 @@ manuf_listings.className = "manuf_list";
 var target = "";
 
 function check_login(){
-    //
     let logged_in = get_cookie("user") != "" ? true : false;
     if (logged_in) {
         console.log("logged in");
@@ -32,25 +31,15 @@ function get_cookie(str){
         if (c.indexOf(name) == 0) {
           return c.substring(name.length, c.length);
         }
-      }
-      return "";
+    }
+    return "";
 }
 
 function delete_cookie(str){
     document.cookie = str + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; 
 }
+
 (function() {
-    $.ajax("/cars")
-        .done( data => {
-            for(car of data){
-                //console.log(car);
-                add_car_listing(car);
-            }
-        });
-    $.ajax("/manufacturers")
-        .done( data => {
-            add_manuf_listing(data);
-        })
     if(check_login()){
         $("#login-button").html("Profile");
     } else {
@@ -58,60 +47,82 @@ function delete_cookie(str){
     }
 })()
 
+function load_cars(){
+    $.ajax("/cars")
+        .done( data => {
+            add_car_listing(data);
+        });
+}
+
 $("#logo").click(() => {
     target = "landing";
     clear_search();
 });
 
+function load_manufs(){
+    $.ajax("/manufacturers")
+        .done( data => {
+            add_manuf_listing(data);
+        })
+}
 
-function add_car_listing(car) {
-    var card = document.createElement("div");
-    card.className += "listing-card";
-    var title = document.createElement("h3");
-    title.className += "mid";
-    title.textContent = car.manufacturer + " ";
-    title.textContent +=  car.name;
-    var img = document.createElement("img");
-    img.src = "images/placeholder.png";
-    var color = cp();
-    color.textContent = "Color: ";
-    color.textContent += car.color;
-    var y = cp();
-    y.textContent = "Year: "
-    y.textContent += car.year;
-    var avail = cp();
-    avail.textContent = "Available: ";
-    avail.textContent += car.available;
-    var cons = cp();
-    cons.textContent = "Consumption: "
-    cons.textContent += car.consumption;
-    var hp = cp();
-    hp.textContent = "Horsepower: "
-    hp.textContent += car.horsepower;
-    card.appendChild(title);
-    card.appendChild(img);
-    card.appendChild(color);
-    card.appendChild(y);
-    card.appendChild(avail);
-    card.appendChild(cons);
-    card.appendChild(hp);
-    car_listings.appendChild(card);
+function add_car_listing(cars) {
+    car_listings.innerHTML = "";
+    for(car of cars){
+        let card = document.createElement("div");
+        card.className += "listing-card";
+        let title = document.createElement("h3");
+        title.className += "mid";
+        title.textContent = car.manufacturer + " ";
+        title.textContent +=  car.name;
+        let img = document.createElement("img");
+        img.src = "images/placeholder.png";
+        let color = cp();
+        color.textContent = "Color: ";
+        color.textContent += car.color;
+        let y = cp();
+        y.textContent = "Year: "
+        y.textContent += car.year;
+        let avail = cp();
+        avail.textContent = "Available: ";
+        avail.textContent += car.available;
+        let cons = cp();
+        cons.textContent = "Consumption: "
+        cons.textContent += car.consumption;
+        let hp = cp();
+        hp.textContent = "Horsepower: "
+        hp.textContent += car.horsepower;
+        card.appendChild(title);
+        card.appendChild(img);
+        card.appendChild(color);
+        card.appendChild(y);
+        card.appendChild(avail);
+        card.appendChild(cons);
+        card.appendChild(hp);
+        car_listings.appendChild(card);
+    }
+    main.appendChild(car_listings);
 }
 
 function add_manuf_listing(manufs) {
     //console.log(manufs);
-    var list = document.createElement("ul");
+    let list = document.createElement("ul");
     list.className = "outer-list";
     for(m of manufs){
-        var outer_li = document.createElement("li");
-        outer_li.className = "outer-li";
-        var inner_ul = document.createElement("ul");
+        let outer_li = document.createElement("li");
+        outer_li.className             .done((data)=>{
+            console.log(data);
+            console.log("sdad");
+        }).done(status => {
+            console.log(status);
+        })= "outer-li";
+        let inner_ul = document.createElement("ul");
         inner_ul.className = "inner-list";
-        var inner_li_n = document.createElement("li");
+        let inner_li_n = document.createElement("li");
         inner_li_n.className = "li-name";
-        var inner_li_c = document.createElement("li");
+        let inner_li_c = document.createElement("li");
         inner_li_c.className = "li-country";
-        var inner_li_f = document.createElement("li");
+        let inner_li_f = document.createElement("li");
         inner_li_f.className = "li-founded";
         inner_li_n.textContent = m.name;
         inner_li_c.textContent = m.country;
@@ -125,7 +136,7 @@ function add_manuf_listing(manufs) {
     manuf_listings.appendChild(list);
 }
 
-function parse_json() {Profile
+function parse_json() {
     
 }
 
@@ -136,18 +147,38 @@ function cp() {
 $("#list_cars").click(() => {
     target = "cars"
     clear_search();
-    main.appendChild(car_listings);
+    add_car_options();
+    load_cars();
+    $("#submit_car_select").on("click", () => {
+        let sel = document.getElementById("car_select");
+        //console.log(sel.value);
+        if(sel.value == "all-cars"){
+            load_cars();
+            return;
+        }
+        document.cookie = "name=" + sel.value;
+        $.ajax("/manufacturer")
+            .done(data => {
+                //console.log(data);
+                add_car_listing(data);
+            });
+    });
 });
 
 $("#list_manuf").click(() => {
     target = "manufs";
     clear_search();
+    //$("#car_search").on("input", () => {
+    //    let str = $("#car_search").val();
+    //    ls_cars_by_mf(str);
+    //});
+    load_manufs();
     main.appendChild(manuf_listings);
 });
 
 
 $("#search_bar").on("input", function() {
-    console.log($(this).val());
+    //console.log($(this).val());
     reset_classes();
     search(target, $(this).val());
 });
@@ -167,6 +198,9 @@ function search(t, str) {
                     a.className += " hidden";
                 }
             }
+            break;
+        case "manufs":
+            break;
         default:
             break;
     }
@@ -192,7 +226,7 @@ $("#login_submit").click(() => {
 
     console.log("Username: ", login_user, " Password: ", login_pwd);
     if(login_user == "" || login_pwd == ""){
-        show_error("Login failed");
+        show_msg("Login failed");
     } else {
         //log in
         console.log("logging in..");
@@ -201,7 +235,7 @@ $("#login_submit").click(() => {
     }
 });
 
-function show_error(error_msg) {
+function show_msg(error_msg) {
     let perror = $("#alert");
     console.log(error_msg);
     if(error_msg == undefined){
@@ -224,22 +258,105 @@ login_btn.addEventListener("click", () => {
     }
 });
 
-//$("#logout_btn").unbind('click').bind("click", () => {
-//});
-
-
+//logout
 var logout_button = document.getElementById("logout_btn");
-
 logout_button.addEventListener("click", () => {
     logout();
 });
-
 function logout() {
     console.log("logging out..");
     delete_cookie("user");
     location.reload();
 }
+//addcar
+var add_car_button = document.getElementById("add_car_btn");
+add_car_button.addEventListener("click", () => {
+    add_car_form();
+});
+var add_mf_button = document.getElementById("add_mf_btn");
+add_mf_button.addEventListener("click", () => {
+    add_mf_form();
+})
+function add_car_form() {
+    $("#car_modal").show();
+    console.log("ad");
+    $("#car_modal_submit").on("click", () => {
+        //$.post("/addCar", {
+        //    "name": $("#car_modal input")[0] 
+        //})
+        let cname = $("#car_modal input")[0].value;
+        let ccons = $("#car_modal input")[1].value;
+        let ccolor = $("#car_modal input")[2].value;
+        let cmf = $("#car_modal input")[3].value;
+        let cavail = $("#car_modal input")[4].value;
+        let cyear = $("#car_modal input")[5].value;
+        let chp = $("#car_modal input")[6].value;
+        
+        let car = {
+            "name" : cname, 
+            "consumption" :ccons,
+            "color" : ccolor,
+            "manufacturer" : cmf, 
+            "available" : cavail, 
+            "year" : cyear,
+            "horsepower" : chp
+        }
+        $.post("/addCar", car,(data) => {
+            console.log(data); 
+        }).fail( () => {
+            show_msg("Failed to add car!")
+        })
+        $("#car_modal").hide();
+    });
+}
 
+function add_mf_form(){
+    $("#mf_modal").show();
+    console.log("anyad");
+    $("#mf_modal_submit").on("click", () => {
+        
+        let mname = $("#mf_modal input")[0].value;
+        let mcountry = $("#mf_modal input")[1].value;
+        let mdate = $("#mf_modal input")[2].value;
+
+        let mf = {
+            "name" : mname,
+            "country" : mcountry,
+            "founded" : mdate
+        }
+
+        $.post("/addManufacturers", mf ,(data) => {
+            console.log(data); 
+        }).fail( () => {
+            show_msg("Failed to add manufacturer!")
+        })
+
+        console.log(mf);
+        $("#mf_modal").hide();
+    });
+}
+var car_modal = document.getElementById('car_modal');
+window.onclick = function(event) {
+  if (event.target == car_modal) {
+    car_modal.style.display = "none";
+  }
+}
+var mf_modal = document.getElementById("mf_modal");
+window.onclick = function(event) {
+    if (event.target == mf_modal) {
+      mf_modal.style.display = "none";
+    }
+}
+var close_modal1 = document.getElementById("close_modal1");
+close_modal1.addEventListener("click", () => {
+    document.getElementById('car_modal').style.display='none';
+});
+var close_modal2 = document.getElementById("close_modal2");
+close_modal2.addEventListener("click", () => {
+    document.getElementById('mf_modal').style.display='none';
+});
+
+//
 function get_cars(man) {
     document.cookie = "name=" + man;
     //console.log(document.cookie);
@@ -248,3 +365,49 @@ function get_cars(man) {
     });
     //delete_cookie('name');
 }
+
+var search_form = `
+<div class="car_search">
+    <select id="car_select">
+        <option value="all-cars">Select a car manufacturer</option>
+    </select>
+    <input type="button" value="Submit" id="submit_car_select">
+</div>
+`
+function add_car_options(){
+    main.innerHTML += search_form;
+    let sel = document.getElementById("car_select");
+    $.ajax("/manufacturerNames")
+        .done(data => {
+            for(car of data){
+                console.log(car);
+                let opt = document.createElement("option");
+                opt.textContent = car;
+                sel.appendChild(opt);
+            }
+        });
+}
+/*
+function ls_cars_by_mf(str){
+    console.log(str);
+    document.getElementById("suggest-list").innerHTML = "";
+    $.ajax("/manufacturerNames")
+        .done(data => {
+            for(car of data){
+                if(str != ""){
+                    if(car.toLowerCase().includes(str.toLowerCase())){
+                        document.getElementById("suggest-list").appendChild(mk_search_item(car));
+                    }
+                }
+            }
+        });
+}
+
+function mk_search_item(name){
+    var li = document.createElement("li");
+    li.className = "suggest-item";
+    li.textContent = name;
+    console.log(li);
+    return li;
+}
+*/
